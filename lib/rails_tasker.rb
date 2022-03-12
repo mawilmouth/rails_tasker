451c10fe60519bb7a59d63965d7d9a425e32fb24
Michael Wilmouth <wilmouthworks@gmail.com>
2022-03-11 21:19:15 -0500

    Add RailsTaker::RailTie

--- lib/rails_tasker.rb
+++ lib/rails_tasker.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'rails_tasker/version'
+require_relative "rails_tasker/railtie" if defined?(Rails::Railtie)
 
 module RailsTasker
 end
---
+++ lib/rails_tasker/railtie.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module RailsTasker
+  class RailTie < Rails::RailTie
+  end
+end
