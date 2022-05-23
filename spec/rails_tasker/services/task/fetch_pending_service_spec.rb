60d9126e7aee1bf2aa1b4c036077b011b5a34c29
Michael Wilmouth <wilmouthworks@gmail.com>
2022-05-23 17:00:04 -0400

    Add RailsTasker::Task::FetchPendingService

---
+++ spec/rails_tasker/services/task/fetch_pending_service_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_tasker/services/task/fetch_pending_service'
+require 'rails_tasker/serviceable'
+require 'rails_tasker/task_file'
+
+RSpec.describe RailsTasker::Task::FetchPendingService do
+  before do
+    stub_const(
+      "RailsTasker::Task::FILE_LOCATION",
+      'examples/lib/rails_tasker/tasks/*.rb',
+    )
+  end
+
+  it 'includes Serviceable' do
+    expect(described_class.ancestors).to include RailsTasker::Serviceable
+  end
+
+  describe '#call' do
+    it 'returns an Array' do
+      expect(described_class.call).to be_a Array
+    end
+
+    it 'returns the expected number of files' do
+      expect(described_class.call.length).to eq 1
+    end
+
+    it 'returns task files' do
+      expect(described_class.call).to all be_a RailsTasker::TaskFile
+    end
+  end
+end
