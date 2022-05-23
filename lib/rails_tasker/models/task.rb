83b28469e58bbe8e8191022e4b19b9859eed3f1d
Michael Wilmouth <wilmouthworks@gmail.com>
2022-05-23 16:44:12 -0400

    Add completion check to RailsTasker::Task

--- lib/rails_tasker/models/task.rb
+++ lib/rails_tasker/models/task.rb
@@ -3,5 +3,11 @@
 module RailsTasker
   class Task < ActiveRecord::Base
     self.table_name = 'rails_tasker_tasks'
+
+    FILE_LOCATION = File.join(Rails.root, 'lib/rails_tasker/tasks/*.rb')
+
+    def self.completed_task?(version:)
+      !all.find_by(version: version).nil?
+    end
   end
 end
--- spec/rails_tasker/models/task_spec.rb
+++ spec/rails_tasker/models/task_spec.rb
@@ -4,9 +4,37 @@ require 'spec_helper'
 require 'rails_tasker/models/task'
 
 RSpec.describe RailsTasker::Task do
+  describe 'FILE_LOCATION' do
+    it 'returns the expected file path' do
+      expect(described_class::FILE_LOCATION).to(
+        eq './lib/rails_tasker/tasks/*.rb'
+      )
+    end
+  end
+  
   describe '#table_name' do
     it 'sets the expected table name' do
       expect(described_class.table_name).to eq 'rails_tasker_tasks'
+    end
+  end
+
+  describe '#completed_task?' do
+    let(:call) { described_class.completed_task?(version: 1234) }
+
+    context 'the record exists' do
+      before do
+        allow(described_class).to receive(:all_result).and_return([5678, 1234])
+      end
+
+      it 'returns true' do
+        expect(call).to eq true
+      end
+    end
+
+    context 'the record does not exist' do
+      it 'returns false' do
+        expect(call).to eq false
+      end
     end
   end
 end
