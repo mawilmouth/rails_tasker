5fc7e567430dbf132c6f3dc136bccbb24f075f7c
Michael Wilmouth <wilmouthworks@gmail.com>
2022-06-16 17:46:26 -0400

    Add console output to run service

--- lib/rails_tasker/services/task/run_pending_service.rb
+++ lib/rails_tasker/services/task/run_pending_service.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'colorize'
 require 'rails_tasker/services/task/fetch_pending_service'
 require 'rails_tasker/serviceable'
 
@@ -9,13 +10,20 @@ module RailsTasker
       include Serviceable
 
       def call
-        pending_tasks.map(&:call).length
+        puts "Running pending tasks...\n".cyan
+        pending_tasks.map do |task|
+          puts(
+            "Running Version: #{task.timestamp} Name: #{task.task_name}".magenta
+          )
+          print 'Output: '.green, task.call, "\n\n"
+        end
+        puts "Successfully ran #{pending_tasks.length} tasks...".green
       end
 
       private
 
       def pending_tasks
-        @pending_tasks ||= FetchPendingService.call.to_a
+        @pending_tasks ||= FetchPendingService.call
       end
     end
   end
