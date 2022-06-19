8cbd6a9d35d9075e2297dfd89c7eb1bd82438a36
Michael Wilmouth <wilmouthworks@gmail.com>
2022-06-19 08:31:01 -0400

    Load rake tasks and models in Railtie

--- lib/rails_tasker/railtie.rb
+++ lib/rails_tasker/railtie.rb
@@ -2,8 +2,16 @@
 
 module RailsTasker
   class Railtie < Rails::Railtie
+    rake_tasks do
+      load 'rails_tasker/rake_tasks/run.rake'
+    end
+
+    initializer 'load_task_record_models' do
+      require 'rails_tasker/models/task'
+    end
+
     generators do
-      require_relative 'generators/install/install_generator.rb'
+      require 'rails_tasker/generators/install/install_generator'
     end
   end
 end
