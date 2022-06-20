5d4a00cd048cfc43e45c09be9777f0ddec355e01
Michael Wilmouth <wilmouthworks@gmail.com>
2022-06-20 18:33:50 -0400

    Add Task::FetchService

---
+++ spec/rails_tasker/services/task/fetch_service_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_tasker/services/task/fetch_service'
+require 'rails_tasker/serviceable'
+require 'rails_tasker/task_file'
+
+RSpec.describe RailsTasker::Task::FetchService do
+  before do
+    stub_const(
+      "RailsTasker::Task::FILE_LOCATION",
+      'examples/lib/rails_tasker/tasks/*.rb',
+    )
+  end
+
+  it 'includes Serviceable' do
+    expect(described_class.ancestors).to include RailsTasker::Serviceable
+  end
+
+  describe '#call' do
+    let(:call) { described_class.call }
+
+    it 'returns the expected number of files' do
+      expect(call.length).to eq 3
+    end
+
+    it 'returns task files' do
+      expect(call).to all be_a RailsTasker::TaskFile
+    end
+
+    it 'returns the filtered/sorted task files' do
+      expect(call.map(&:timestamp)).to eq %w[3 1234 12345]
+    end
+  end
+end
